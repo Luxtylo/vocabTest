c1ead7682d517acf1fb6bb6343db8630d9d63eee
George Bryant <gb@gbryant.co.uk>
2013-11-26 20:36:31 +0000

    Added hideAnswers.py to change test.txt into a version with hashed English words. The English answers given by the students will be hashed and compared to the hash

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ vocabTest
 =========
 
 Python program for a French teacher to create vocabulary tests for his students, and for the students to take them
+
+The teacher's program needs to be able to create tests, which are stored somewhere the students can access. The tests will always be a French word or phrase and its English translation. To avoid confusion, the answers should be compared with the questions in lowercase.
+
+The students' program should be able to download a file created by the teacher's program (so a small server would be needed to serve up the files). It will list the French words, and the student will need to correctly input their translation. There will always be 10 questions in the test, which is few enough to show them all at once. After the student has put in their answers, the program should give them a total score.
+
+Both sides of the program need a GUI. The creation program will not be distributed to the students, and so does not require a login, but the student part needs a secure login option to prevent students impersonating each other. The student client should send their result to the server.
---
+++ hideAnswers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+vocabTest - a test-making and taking program.
+
+Copyright (C) 2013  George Bryant
+
+This program is free software:
+    you can redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software Foundation,
+    either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+You should have received a copy of the GNU General Public License along with
+    this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import hashlib
+
+testFile = open("tests/test.txt", "r")
+
+wordList = list()
+
+
+def getCutOffMD5(english):
+    return hashlib.md5(english).hexdigest()[:8]
+
+lineNum = 1
+for line in testFile:
+    if lineNum == 1:
+        title = line
+    elif lineNum == 2:
+        dateString = line
+    else:
+        frenchWord = line.split("|")[0]
+        englishWord = line.split("|")[1].lower()
+        englishHash = getCutOffMD5(englishWord.encode())
+        translationTuple = (frenchWord, englishHash)
+        wordList.append(translationTuple)
+    lineNum += 1
+
+print(wordList)
